92946c57f434cfa590ecaec7b0ef61d14eae63ba
TJ P <mail@tjpalanca.com>
2023-02-20 03:54:14 +0000

    Fix docs

--- notiontools/_modidx.py
+++ notiontools/_modidx.py
@@ -5,4 +5,4 @@ d = { 'settings': { 'branch': 'master',
                 'doc_host': 'https://tjpalanca.com',
                 'git_url': 'https://github.com/tjpalanca/notiontools.git',
                 'lib_path': 'notiontools'},
-  'syms': {}}
+  'syms': {'notiontools.kindle': {}}}
